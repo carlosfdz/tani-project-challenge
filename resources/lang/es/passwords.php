b3a5c30bccc0b3f81f17b7c416925d9ee48e7ef2
Juan Carlos Carrera Fernandez <fernandez.isc@gmail.com>
2021-09-03 15:51:42 -0500

    Lang files for validation and translation examples.

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+     */
+
+    'password' => 'La contraseña debe tener 6 caracteres o más y coincidir con la confirmación.',
+    'user' => 'No pudimos encontrar un usuario con ese e-mail',
+    'token' => 'El token para restaurar la password es inválido',
+    'sent' => 'Te enviamos un correo para restaurar tu contraseña',
+    'reset' => 'Tu contraseña ha sido restaurada',
+
+];
